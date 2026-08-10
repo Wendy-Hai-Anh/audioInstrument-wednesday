e3ad369edfc380ebc45f20831740adf6c097d745
Patrick McMahon <patrick.mcmahon@rmit.edu.au>
2026-08-10 13:33:13 +1000

    run through of wednesday's class

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
-// find our test button
-const testButton = document.getElementById('test-button');
+const introDialog = document.getElementById("intro-dialog");
+const introDialogCloseButton = document.getElementById("intro-dialog-close-button");
+// console.log(introDialog);
+
+
+///// Intro Modal
+introDialog.showModal();
+introDialogCloseButton.addEventListener("click", function closeDialog() {
+    introDialog.close();
+});
+
+introDialog.addEventListener("close", toneInit);
+
+async function toneInit(){
+    await Tone.start();
+    // find our test button
+    const testButton = document.getElementById('test-button');
 // init our synth
-const synth = new Tone.Synth().toDestination();
+    const synth = new Tone.Synth().toDestination();
 
 // do something when this button is clicked
-testButton.addEventListener("click", playNote);
+    testButton.addEventListener("click", playNote);
 
 // function that runs when button is clicked
-function playNote(){
-    //play a note for a duration
-    synth.triggerAttackRelease("c4", "8n");
+    function playNote(){
+        //play a note for a duration
+        synth.triggerAttackRelease("c4", "8n");
+    }
 }
+
+
